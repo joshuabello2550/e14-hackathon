4ebbc21fabdcad836cb17903924e338d1ad8c565
Joshua Bello <joshuabello2550@gmail.com>
2026-03-01 01:38:47 -0500

    increased endpoint data limit

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ import { getSessionEventsData } from "./api/posthog/sessionEventsQuery.ts";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 app.post("/api/create-file", async (req, res) => {
   const { installationId, owner, repo, content } = req.body;
